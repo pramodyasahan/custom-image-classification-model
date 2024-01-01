96dc5bde6241c67b5f035b80078c321eb5fe92d4
pramodyasahan <sahandil1013@gmail.com>
2024-01-01 21:04:26 +0530

    feat: predict new images

--- CNNImageClassifier/ImageClassifier.py
+++ CNNImageClassifier/ImageClassifier.py
@@ -1,8 +1,11 @@
 import tensorflow as tf
 import matplotlib.pyplot as plt
+import os
+import cv2
 import numpy as np
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Conv2D, Flatten, MaxPooling2D
+from tensorflow.keras.metrics import Precision, Recall, BinaryAccuracy
 
 data = tf.keras.utils.image_dataset_from_directory('data')
 data = data.map(lambda x, y: (x / 255, y))
@@ -28,3 +31,42 @@ model.add(Flatten())
 model.add(Dense(256, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
 model.compile('adam', 'binary_crossentropy', metrics=['accuracy'])
+print(model.summary())
+
+logdir = 'logs'
+tensorboard = tf.keras.callbacks.TensorBoard(log_dir=logdir)
+hist = model.fit(train, epochs=20, validation_data=val, callbacks=[tensorboard])
+
+fig = plt.figure()
+plt.plot(hist.history['loss'], color='teal', label='loss')
+plt.plot(hist.history['val_loss'], color='orange', label='val_loss')
+fig.suptitle('Loss', fontsize=20)
+plt.legend(loc='upper left')
+plt.show()
+
+pre = Precision()
+re = Recall()
+acc = BinaryAccuracy()
+
+for batch in test.as_numpy_iterator():
+    X, y = batch
+    y_pred = model.predict(X)
+    pre.update_state(y, y_pred)
+    re.update_state(y, y_pred)
+    acc.update_state(y, y_pred)
+
+print(f'Precision: {pre.result().numpy()}, Recall: {re.result().numpy()}, Accuracy: {acc.result().numpy()}')
+
+img = cv2.imread('cat.jpg')
+plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+plt.show()
+
+resize = tf.image.resize(img, (256, 256))
+plt.imshow(resize.numpy().astype(int))
+plt.show()
+
+y_pred = model.predict(np.expand_dims(resize / 255.0, 0))
+if y_pred > 0.5:
+    print("Predicted class is Sad")
+else:
+    print("Predicted class is Happy")
