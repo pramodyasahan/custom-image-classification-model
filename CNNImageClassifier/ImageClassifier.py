343df52a44e81f5b63d0e5cb7a176ca65fa20681
pramodyasahan <sahandil1013@gmail.com>
2024-01-01 16:27:02 +0530

    feat: split the data set

--- CNNImageClassifier/ImageClassifier.py
+++ CNNImageClassifier/ImageClassifier.py
@@ -3,5 +3,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 data = tf.keras.utils.image_dataset_from_directory('data')
-data_iterator = data.as_numpy_iterator()
-batch = data_iterator.next()
+data = data.map(lambda x, y: (x / 255, y))
+data.as_numpy_iterator().next()
+
+train_size = int(len(data) * 0.7) + 2
+val_size = int(len(data) * 0.2)
+test_size = int(len(data) * 0.1)
+print(len(data))
+print(train_size + val_size + test_size)
+
+train = data.take(train_size)
+val = data.skip(train_size).take(val_size)
+test = data.skip(train_size + val_size).take(test_size)
