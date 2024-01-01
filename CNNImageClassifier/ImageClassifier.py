8e2f15ac69b8286b431747dbf939f3d566e59616
pramodyasahan <sahandil1013@gmail.com>
2024-01-01 16:59:31 +0530

    feat: build the model

--- CNNImageClassifier/ImageClassifier.py
+++ CNNImageClassifier/ImageClassifier.py
@@ -1,6 +1,8 @@
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Conv2D, Flatten, MaxPooling2D
 
 data = tf.keras.utils.image_dataset_from_directory('data')
 data = data.map(lambda x, y: (x / 255, y))
@@ -9,9 +11,20 @@ data.as_numpy_iterator().next()
 train_size = int(len(data) * 0.7) + 2
 val_size = int(len(data) * 0.2)
 test_size = int(len(data) * 0.1)
-print(len(data))
-print(train_size + val_size + test_size)
 
 train = data.take(train_size)
 val = data.skip(train_size).take(val_size)
 test = data.skip(train_size + val_size).take(test_size)
+
+model = Sequential()
+model.add(Conv2D(16, (3, 3), 1, activation='relu', input_shape=(256, 256, 3)))
+model.add(MaxPooling2D())
+model.add(Conv2D(32, (3, 3), 1, activation='relu'))
+model.add(MaxPooling2D())
+model.add(Conv2D(16, (3, 3), 1, activation='relu'))
+model.add(MaxPooling2D())
+model.add(Flatten())
+
+model.add(Dense(256, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+model.compile('adam', 'binary_crossentropy', metrics=['accuracy'])
